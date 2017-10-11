b6beece92330a1be1501c018a8632b9245e99963
Sambhav Nidamarty <sambhavdutt@gmail.com>
2017-10-11 17:33:45 -0400

    [FAB-5546] Configure Makefile in fabric-test
    This patch creates a Makefile to run ci tests on fabric-test repository.
    This patch removes the dependency on CI configurable scripts instead it
    maintains common script to execute all the targets from makefile.
    
    make ci-smoke and make ci-daily
    
    Change-Id: I64b5444b6559644e7dcaebfec29cda44c46fc799
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>
    Signed-off-by: RameshBabu Thoomu <rameshbabu.thoomu@gmail.com>

---
+++ pre_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+set -o pipefail
+
+# Install nvm to install multi node versions
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# shellcheck source=/dev/null
+export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/dev/null
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Install nodejs version 8.4.0
+nvm install 8.4.0 || true
+
+# use nodejs 8.4.0 version
+nvm use 8.4.0
+
+echo "npm version ======>"
+npm -v
+echo "node version =======>"
+node -v
