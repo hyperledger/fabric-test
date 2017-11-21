300573a7debfda37555863db34596561ec737059
Dongming <dming@us.ibm.com>
2017-11-21 10:32:39 -0500

    [FAB-3810] TPS queries 2 threads CouchDB
    move directory scenarios under PTE/CITest
    
    Change-Id: I2c65048ebd03e96eee08cb6274257c9b4cba6773
    Signed-off-by: Dongming <dming@us.ibm.com>

--- tools/PTE/CITest/scenarios/FAB-3807-4i.sh
+++ tools/PTE/CITest/scenarios/FAB-3807-4i.sh
@@ -13,14 +13,16 @@ PREFIX="result"   # result log prefix
 
 cd ~/gopath/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scripts
 
-#### Lauch network and execute testcase
-#### FAB-3807-4i: 4 invokes, golevelDB
+#### Lauch network and synch-up ledger
 ./test_driver.sh -n -m FAB-3807-4i -p -c samplecc -t FAB-3835-4q
+#### remove PTE log from synch-up ledger run
 rm -f ../Logs/FAB-3835-4q*.log
+#### execute testcase FAB-3807-4i: 4 invokes, golevelDB
 ./test_driver.sh -t FAB-3807-4i
+#### gather TPS from docker containers
 ./get_peerStats.sh -r FAB-3807-4i -p peer0.org1.example.com peer0.org2.example.com -n $PREFIX -o $CWD -v
 
-#### execute testcase
-#### FAB-3835-4q: 4 queries, golevelDB
+#### execute testcase FAB-3835-4q: 4 queries, golevelDB
 ./test_driver.sh -t FAB-3835-4q
+#### gather TPS from PTE log
 grep Summary ../Logs/FAB-3835-4q*.log | grep "QUERY" >> $CWD/$PREFIX"_FAB-3807-4i.log"
--- tools/PTE/CITest/scenarios/FAB-3808-2i.sh
+++ tools/PTE/CITest/scenarios/FAB-3808-2i.sh
@@ -13,15 +13,17 @@ PREFIX="result"   # result log prefix
 
 cd ~/gopath/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scripts
 
-#### Lauch network and execute testcase
-#### FAB-3808-2i: 2 invokes, golevelDB
+#### Lauch network and synch-up ledger
 ./test_driver.sh -n -m FAB-3808-2i -p -c samplecc -t FAB-3811-2q
+#### remove PTE log from synch-up ledger run
 rm -f ../Logs/FAB-3811-2q*.log
+#### execute testcase FAB-3808-2i: 2 invokes, golevelDB
 ./test_driver.sh -t FAB-3808-2i
+#### gather TPS from docker containers
 ./get_peerStats.sh -r FAB-3808-2i -p peer0.org1.example.com peer0.org2.example.com -n $PREFIX -o $CWD -v
 
-#### execute testcase
-#### FAB-3811-2q: 2 queries, golevelDB
+#### execute testcase FAB-3811-2q: 2 queries, golevelDB
 ./test_driver.sh -t FAB-3811-2q
+#### gather TPS from PTE log
 grep Summary ../Logs/FAB-3811-2q*.log | grep "QUERY" >> $CWD/$PREFIX"_FAB-3808-2i.log"
 
