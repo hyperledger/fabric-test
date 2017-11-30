3583a099937953f70ed9fa5095e41f8c5e1c3faa
Dongming <dming@us.ibm.com>
2017-11-30 16:19:19 -0500

    [FAB-4038] TPS 2 threads invokes sample_cc nodeJS
    This includes network setup and PTE scritps for
    [FAB-4038] TPS 2 threads invokes LevelDB
    [FAB-4036] TPS 2 threads queries LevelDB
    
    Change-Id: Ie51eeff96b533d97025869ac830c54db233a26d8
    Signed-off-by: Dongming <dming@us.ibm.com>

---
+++ tools/PTE/CITest/scenarios/FAB-4038-2i.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+########## CI test ##########
+
+CWD=$PWD
+PREFIX="result"   # result log prefix
+
+#### Lauch network and synch-up ledger
+cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scripts
+./test_driver.sh -n -m FAB-4038-2i -p -c samplejs -t FAB-4036-2q
+#### remove PTE log from synch-up ledger run
+rm -f ../Logs/FAB-4036-2q*.log
+#### execute testcase FAB-4038-2i: 2 invokes, golevelDB
+./test_driver.sh -t FAB-4038-2i
+#### gather TPS from docker containers
+./get_peerStats.sh -r FAB-4038-2i -p peer0.org1.example.com peer0.org2.example.com -n $PREFIX -o $CWD -v
+
+#### execute testcase FAB-4036-2q: 2 queries, golevelDB
+./test_driver.sh -t FAB-4036-2q
+#### gather TPS from PTE log
+grep Summary ../Logs/FAB-4036-2q*.log | grep "QUERY" >> $CWD/$PREFIX"_FAB-4038-2i.log"
+
