f3f29494fde3ae80e5ee2080c61d7e5e823301e8
Surya <suryalnvs@gmail.com>
2017-12-18 13:37:38 -0500

    [FAB-7002] Fixing node command not found in ATD
    Updated the scripts to use node version 8.x in ATD
    Adding ptenetwork.yml that uses the same network configuration as
    PTE.
    
    Change-Id: Icc38836e4256fea55f68541962a2cd870e4591a0
    Signed-off-by: Surya <suryalnvs@gmail.com>

--- tools/ATD/images/pte/testcases.sh
+++ tools/ATD/images/pte/testcases.sh
@@ -10,13 +10,10 @@ echo "##########################"
 echo "STARTING THE PTE CONTAINER"
 echo "##########################"
 
-export PATH=$PATH:/root/.nvm/versions/node/v6.11.4/bin
-
 cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scripts
 
 echo "##################"
 echo "STARTING THE TESTS"
 echo "##################"
-
 ./test_driver.sh -p -c $CHAINCODE -t $TESTCASE
 while ps axg | grep -vw grep | grep -w "node ./pte-execRequest.js" > /dev/null; do sleep 120; done
