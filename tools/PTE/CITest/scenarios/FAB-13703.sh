f91ea7dc86b35835f20242a7f57c59a00583838c
Dongming <dongming@ibm.com>
2019-02-01 11:06:54 -0500

    [FAB-13703] PTE raft test: 1 thread 3 channels
    PTE RAFT testcase:
    1 thread X 3 channels Latency mode
    
    Change-Id: Ife39f1156c7cc8177fef83725a311c9e5b9230af
    Signed-off-by: Dongming <dongming@ibm.com>

---
+++ tools/PTE/CITest/scenarios/FAB-13703.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# testcase: FAB-13703
+# channels: 3
+# org: 1
+# thread: 1 (1 per org)
+# tx 10,000 per thread
+# traffic mode: Latency
+
+myTESTCASE="PTEScaleTest"
+myLog="FAB-13703"
+
+myCC="samplecc"
+myTXMODE="Latency"
+myNORG=1
+
+myNREQ=10000
+
+myMinChan=3
+myMaxChan=3
+myChanIncr=1
+myMinTh=1
+myMaxTh=1
+myThIncr=1
+
+myKey0=0
+
+CWD=$PWD
+
+FabricTestDir=$GOPATH"/src/github.com/hyperledger/fabric-test"
+PTEDir=$FabricTestDir"/tools/PTE"
+LOGDir=$PTEDir"/CITest/Logs"
+
+CIpteReport=$LOGDir/$myTESTCASE"-pteReport.log"
+echo "CIpteReport=$CIpteReport"
+
+if [ -e $CIpteReport ]; then
+    rm -f $CIpteReport
+fi
+
+# channels loop
+for (( myNCHAN = $myMinChan; myNCHAN <= $myMaxChan; myNCHAN+=$myChanIncr )); do
+    # threads loop
+    for (( myNTHREAD = $myMinTh; myNTHREAD <= $myMaxTh; myNTHREAD+=$myThIncr )); do
+        cd $CWD
+        set -x
+        ./runScaleTraffic.sh  -a $myCC --nchan $myNCHAN --norg $myNORG --nproc $myNTHREAD --nreq $myNREQ --keystart $myKey0 --txmode $myTXMODE -i
+        CMDResult="$?"
+        set +x
+        if [ $CMDResult -ne "0" ]; then
+            echo "Error: Failed to execute runScaleTraffic.sh"
+            exit 1
+        fi
+        myKey0=$(( myKey0+myNREQ ))
+    done
+done
+
+mv $CIpteReport $LOGDir/$myLog"-pteReport.log"
+
+exit 0
