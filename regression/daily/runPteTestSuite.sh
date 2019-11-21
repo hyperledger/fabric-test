514b5faed8688c1abc9525edead62eb1726c2031
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-11-21 15:33:30 -0500

    [FAB-16222] Convert daily pte test driver to go
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I62dd214533a4b93498212e069805029f7af1b682

--- regression/daily/runPteTestSuite.sh
+++ regression/daily/runPteTestSuite.sh
@@ -12,24 +12,28 @@ echo "========== System Test Performance tests using PTE and NL tools..."
 cd $FabricTestDir/tools/PTE
 
 archivePTE() {
-if [ ! -z $GERRIT_BRANCH ] && [ ! -z $WORKSPACE ]; then
-# GERRIT_BRANCH is a Jenkins parameter and WORKSPACE is a Jenkins directory.This function is used only when the test is run in Jenkins to archive the log files.
-    echo "------> Archiving generated logs"
-    rm -rf $WORKSPACE/archives
-    mkdir -p $WORKSPACE/archives/PTE_Test_Logs
-    cp $FabricTestDir/tools/PTE/CITest/Logs/*.log $WORKSPACE/archives/PTE_Test_Logs/
-    mkdir -p $WORKSPACE/archives/PTE_Test_XML
-    cp $FabricTestDir/regression/daily/*.xml $WORKSPACE/archives/PTE_Test_XML/
-fi
+    if [ ! -z $GERRIT_BRANCH ] && [ ! -z $WORKSPACE ]; then
+        # GERRIT_BRANCH is a Jenkins parameter and WORKSPACE is a Jenkins directory.This function is used only when the test is run in Jenkins to archive the log files.
+        echo "------> Archiving generated logs"
+        rm -rf $WORKSPACE/archives
+        mkdir -p $WORKSPACE/archives/PTE_Test_Logs
+        cp $FabricTestDir/tools/PTE/CITest/Logs/*.log $WORKSPACE/archives/PTE_Test_Logs/
+        mkdir -p $WORKSPACE/archives/PTE_Test_XML
+        cp $FabricTestDir/regression/daily/*.xml $WORKSPACE/archives/PTE_Test_XML/
+    fi
 }
 
-npm install
-  if [ $? != 0 ]; then
-     echo "------> Failed to install npm. Cannot run pte test suite."
-     exit 1
-  else
-     echo "------> Successfully installed npm."
-  fi
+cd $FabricTestDir/tools/PTE
+if [ ! -d "node_modules" ];then
+    npm config set prefix ~/npm
+    npm install
+    if [ $? != 0 ]; then
+        echo "FAILED: Failed to install npm. Cannot run pte test suite."
+        exit 1
+    else
+        echo "Successfully installed npm."
+    fi
+fi
 
-cd $DAILYDIR && py.test -v --junitxml results_systest_pte.xml systest_pte.py && echo "------> PTE tests completed"
+cd $DAILYDIR && ginkgo -v && echo "------> PTE tests completed"
 archivePTE
