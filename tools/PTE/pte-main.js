317a562bc39e2c03642b82ecd287d8f6bb917b62
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-12-06 09:51:04 -0500

    [FAB-17197] Printing PTE test counters with error
    PTE is not printing the test counters if the test
    fails. This patch will fix that issue
    
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>

--- tools/PTE/pte-main.js
+++ tools/PTE/pte-main.js
@@ -1984,7 +1984,7 @@ async function performance_main() {
                             } else {
                                 output["Test Result"] = "FAIL"
                                 logger.info('[performance_main] Test Output:', JSON.stringify(output, null, 4));
-                                throw new Error('[performance_main] Test ran, but failed with errors. Exiting... \n pteReport:', JSON.stringify(output, null, 4));
+                                throw new Error('[performance_main] Test ran, but failed with errors. Exiting... \n pteReport: %s', JSON.stringify(output, null, 4));
                             }
 
                         }
