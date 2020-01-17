cbbcc3b42c46a8827164903d9160340e6fa401f8
Brett Logan <brett.t.logan@ibm.com>
2020-01-17 09:19:08 -0500

    [FAB-17404] Add Windows and Darwin Binaries to Artifactory (#51)
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/scripts/trigger_publish_artifacts.sh
+++ ci/scripts/trigger_publish_artifacts.sh
@@ -14,16 +14,25 @@ for image in baseos peer orderer ccenv tools ca javaenv nodeenv; do
     docker push "hyperledger-fabric.jfrog.io/fabric-${image}:amd64-latest"
 done
 
-cd "${GOPATH}/src/github.com/hyperledger/fabric/release/linux-amd64/"
-mkdir -p "config"
-cp ../../sampleconfig/*yaml "config"
-tar -czvf "hyperledger-fabric-linux-amd64-${RELEASE}.tar.gz" bin config
-curl -u"${ARTIFACTORY_USERNAME}":"${ARTIFACTORY_PASSWORD}" \
-     -T "./release/linux-amd64/hyperledger-fabric-linux-amd64-${RELEASE}.tar.gz" \
-     "https://hyperledger.jfrog.io/hyperledger/fabric-binaries/hyperledger-fabric-linux-amd64-${RELEASE}.tar.gz"
 
-cd "${GOPATH}/src/github.com/hyperledger/fabric-ca/release/linux-amd64"
-tar -czvf "hyperledger-fabric-ca-linux-amd64-${RELEASE}.tar.gz" bin
-curl -u"${ARTIFACTORY_USERNAME}":"${ARTIFACTORY_PASSWORD}" \
-     -T "./release/linux-amd64/hyperledger-fabric-ca-linux-amd64-${RELEASE}.tar.gz" \
-     "https://hyperledger.jfrog.io/hyperledger/fabric-binaries/hyperledger-fabric-ca-linux-amd64-${RELEASE}.tar.gz"
+for target in linux-amd64 darwin-amd64 windows-amd64; do
+    cd "${GOPATH}/src/github.com/hyperledger/fabric"
+    make "release/${target}"
+
+    cd "release/${target}/"
+    mkdir -p "config"
+    cp ../../sampleconfig/*yaml "config"
+    tar -czvf "hyperledger-fabric-${target}-${RELEASE}.tar.gz" bin config
+    curl -u"${ARTIFACTORY_USERNAME}":"${ARTIFACTORY_PASSWORD}" \
+         -T "hyperledger-fabric-${target}-${RELEASE}.tar.gz" \
+         "https://hyperledger.jfrog.io/hyperledger/fabric-binaries/hyperledger-fabric-${target}-${RELEASE}.tar.gz"
+
+    cd "${GOPATH}/src/github.com/hyperledger/fabric-ca"
+    make "release/${target}"
+
+    cd "release/${target}"
+    tar -czvf "hyperledger-fabric-ca-${target}-${RELEASE}.tar.gz" bin
+    curl -u"${ARTIFACTORY_USERNAME}":"${ARTIFACTORY_PASSWORD}" \
+         -T "hyperledger-fabric-ca-${target}-${RELEASE}.tar.gz" \
+         "https://hyperledger.jfrog.io/hyperledger/fabric-binaries/hyperledger-fabric-ca-${target}-${RELEASE}.tar.gz"
+done
