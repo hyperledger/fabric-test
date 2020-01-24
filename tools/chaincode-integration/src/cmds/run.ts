46c686a74569970ee71182ec7f4395319f431dbb
Andrew Hurt <andrew.hurt1@ibm.com>
2020-01-24 13:51:04 +0000

    [FAB-17388] Add CLI option for specific network
    Signed-off-by: Andrew Hurt <andrew.hurt1@ibm.com>

--- tools/chaincode-integration/src/cmds/run.ts
+++ tools/chaincode-integration/src/cmds/run.ts
@@ -4,6 +4,7 @@ SPDX-License-Identifier: Apache-2.0
 */
 import * as chalk from 'chalk';
 import * as cucumber from 'cucumber';
+import * as events from 'events';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import { Arguments, Argv } from 'yargs';
@@ -45,7 +46,13 @@ const options = {
         default: 'info',
         description: 'Set logging level',
     },
+    network: {
+        alias: 'n',
+        choices: DEFINED_NETWORKS,
+        description: 'Specific network to run',
+    },
     tags: {
+        alias: 't',
         description: 'Specific tags to run',
         type: 'string',
     },
@@ -69,7 +76,42 @@ const cmd: CommandModule = {
         return args.thePromise = new Promise(async (resolve, reject) => {
             const cucumberErrors = [];
 
-            for (const name of DEFINED_NETWORKS) {
+            const networks = args.network ? [args.network as string] : DEFINED_NETWORKS;
+
+            for (const name of networks) {
+                let argv = process.argv.slice(0, 2).concat(cucumberArgs).concat('--tags', `@${name}`).concat('--tags', `not @not-${args.language}`);
+
+                if (global.CHAINCODE_LANGUAGE === 'node') {
+                    argv = argv.concat('--tags', 'not @not-node');
+                }
+
+                if (args.tags) {
+                    argv = argv.concat('--tags', args.tags as string);
+                }
+
+                const cli = new (cucumber as any).Cli({
+                    argv,
+                    cwd: path.resolve(__dirname, '../..'),
+                    stdout: {write: () => null},
+                });
+
+                logger.debug('Calculating test cases to run');
+                const config = await cli.getConfiguration();
+                const testCases = await (cucumber as any).getTestCasesFromFilesystem({
+                    cwd: cli.cwd,
+                    eventBroadcaster: new events(),
+                    featureDefaultLanguage: config.featureDefaultLanguage,
+                    featurePaths: config.featurePaths,
+                    order: config.order,
+                    pickleFilter: new (cucumber as any).PickleFilter(config.pickleFilterOptions),
+                });
+                logger.debug(`Found ${testCases} test cases`);
+
+                if (testCases.length === 0) {
+                    logger.info('No features to run for network', name);
+                    continue;
+                }
+
                 logger.info(chalk.cyan(`Creating network ${name}`));
 
                 const network = new Network(name);
@@ -101,22 +143,6 @@ const cmd: CommandModule = {
                     reject(err);
                     return;
                 }
-
-                let argv = process.argv.slice(0, 2).concat(cucumberArgs).concat('--tags', `@${name}`).concat('--tags', `not @not-${args.language}`);
-
-                if (global.CHAINCODE_LANGUAGE === 'node') {
-                    argv = argv.concat('--tags', 'not @not-node');
-                }
-
-                if (args.tags) {
-                    argv = argv.concat('--tags', args.tags as string);
-                }
-
-                const cli = new (cucumber as any).Cli({
-                    argv,
-                    cwd: path.resolve(__dirname, '../..'),
-                    stdout: {write: () => null},
-                });
 
                 logger.info(chalk.magenta('Running cucumber tests'));
 
