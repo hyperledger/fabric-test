45799a2ee4eefa49ae705cc57ed415270c35d60a
Surya Lanka <suryalnvs@gmail.com>
2020-02-10 10:12:26 -0500

    [FAB-17203] Check test status in smoke and daily
    Changes to daily/runPteTestSuite.sh, smoke/runSmokeTestSuite.sh
    will enable to verify the test run status and return status code
    accordingly.
    
    Removed the following: execution of PTE/NL test scenario from
    smoke/runSmokeTestSuite.sh and publishing logs from PTE/NL test
    in ci/azure-pipelines.yml.
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>

--- regression/daily/runPteTestSuite.sh
+++ regression/daily/runPteTestSuite.sh
@@ -10,18 +10,6 @@ DAILYDIR="$FabricTestDir/regression/daily"
 echo "========== System Test Performance tests using PTE and NL tools..."
 cd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
 
-archivePTE() {
-    if [ ! -z $GERRIT_BRANCH ] && [ ! -z $WORKSPACE ]; then
-        # GERRIT_BRANCH is a Jenkins parameter and WORKSPACE is a Jenkins directory.This function is used only when the test is run in Jenkins to archive the log files.
-        echo "------> Archiving generated logs"
-        rm -rf $WORKSPACE/archives
-        mkdir -p $WORKSPACE/archives/PTE_Test_Logs
-        cp $FabricTestDir/tools/PTE/CITest/Logs/*.log $WORKSPACE/archives/PTE_Test_Logs/
-        mkdir -p $WORKSPACE/archives/PTE_Test_XML
-        cp $FabricTestDir/regression/daily/*.xml $WORKSPACE/archives/PTE_Test_XML/
-    fi
-}
-
 cd $FabricTestDir/tools/PTE
 if [ ! -d "node_modules" ];then
     npm config set prefix ~/npm
@@ -34,5 +22,13 @@ if [ ! -d "node_modules" ];then
     fi
 fi
 
-cd $DAILYDIR && GO111MODULE=on  ginkgo -v && echo "------> PTE tests completed"
-archivePTE
+cd $DAILYDIR && GO111MODULE=on ginkgo -v
+StatusPteNL=$?
+
+if [ $StatusPteNL == 0 ]; then
+    echo "------> PTE/NL tests completed"
+else
+    echo "------> PTE/NL tests failed with above errors"
+    exit 1
+fi
+cp $FabricTestDir/tools/PTE/CITest/Logs/*.log $DAILYDIR
