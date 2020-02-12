9aace54a743628fc5d0854a093ffeba9cd11b7a9
Brett Logan <brett.t.logan@ibm.com>
2020-02-12 09:55:34 -0500

    [FAB-17496] Remove Fabric
    Remove the fabric submodule, it is no longer used
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- scripts/pullBinaries.sh
+++ scripts/pullBinaries.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 
 REPO=$1
 # Set the working directory
-WD=$(cd `dirname $0` && pwd)/..
-cd "$WD"
+WD=$(cd `dirname $0`/.. && pwd)
+cd ${WD}
 
 RELEASE_VERSION=${RELEASE_VERSION:=latest}
 
 # Get the arch value
 ARCH=$(arch)
-if [[ "$ARCH" = "x86_64" ]]; then
+if [[ "${ARCH}" = "x86_64" ]]; then
     ARCH=linux-amd64
 elif [[ "$ARCH" = "i386" ]]; then
     ARCH=darwin-amd64
@@ -29,6 +29,7 @@ pullBinary() {
     echo
     ARTIFACTORY_URL=https://hyperledger.jfrog.io/hyperledger/fabric-binaries/hyperledger-${repo}-${ARCH}-${RELEASE_VERSION}.tar.gz
     curl "${ARTIFACTORY_URL}" | tar -xvz
+    rm -rf config
     echo "Finished pulling $repo..."
     echo
   done
@@ -52,26 +53,5 @@ fabric-ca)
   ;;
 esac
 
-#Set the PATH to the bin directory in order to execute the correct binaries
-export PATH=${WD}/bin:$PATH
-
-# Show the results
-ls -l "$WD"
-echo
-echo "In the bin dir..."
-ls -l "$WD"/bin/*
-echo
-echo "$PATH"
-
-
-############################################################
-# These should be removed once we retire  legacy daily PTE #
-############################################################
-
-# Copy new binaries to the exported PATH bin dir
-mkdir -p "$WD"/../fabric/.build/bin
-cp -r "$WD"/bin/* "$WD"/../fabric/.build/bin/
-
-# PTE looks for binaries in fabric submodule dir
-mkdir -p "$WD"/fabric/.build/bin
-cp -r "$WD"/bin/* "$WD"/fabric/.build/bin/
+echo "In the ${WD}/bin dir..."
+ls -l ${WD}/bin
