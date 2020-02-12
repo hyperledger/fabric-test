c94d539e2c0e5c9a41f71e25eff36097a2691eed
Surya Lanka <suryalnvs@gmail.com>
2020-02-12 16:14:58 -0500

    [FAB-17485] Basic network test suite
    Adding basic network test suite which includes
    launching network with 1 organization with 1 peer,
    1 raft node, creates 1 channel, joins peer to channel,
    install and instantiate samplecc and map_private chaincode,
    sends 10 invokes to channel per samplecc and map_private
    chaincode and 10 queries for both chaincodes
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>

--- regression/daily/runPteTestSuite.sh
+++ regression/daily/runPteTestSuite.sh
@@ -8,17 +8,23 @@ CurrentDirectory=$(cd `dirname $0` && pwd)
 FabricTestDir="$(echo $CurrentDirectory | awk -F'/fabric-test/' '{print $1}')/fabric-test"
 DAILYDIR="$FabricTestDir/regression/daily"
 BAREBONESDIR="$FabricTestDir/regression/barebones"
+BASICDIR="$FabricTestDir/regression/basicnetwork"
 
 echo "========== System Test Barebones tests using PTE and operator tools..."
 cd $BAREBONESDIR && ginkgo -v
 StatusBarebones=$?
 
-if [ $StatusBarebones == 0 ]; then
-    echo "------> Barebones tests completed"
+echo "======== Basic Test Suite using ginkgo and operator tools ========"
+cd $BASICDIR && ginkgo -v
+StatusBasicTestSuite=$?
+
+if [ $StatusBarebones == 0 ] && [ $StatusBasicTestSuite == 0 ]; then
+    echo "------> Tests in daily bucket completed"
 else
-    echo "------> Barebones tests failed with above errors"
+    echo "------> Tests in daily bucket failed with above errors"
     exit 1
 fi
+
 # save barebones test result
 tCurr=`date +%Y%m%d`
 cp $FabricTestDir/regression/barebones/barebones-pteReport.log $DAILYDIR"/barebones-pteReport-"$tCurr".log"
