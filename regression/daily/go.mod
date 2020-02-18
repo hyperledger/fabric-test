2b6716895f3d95aac606de8874104f055008b7e2
Brett Logan <brett.t.logan@ibm.com>
2020-02-17 19:22:19 -0500

    Add regression wildcard and clean up Makefile
    Adds a `regression/%` target to the Makefile so tests
    can be called in a manner such as : `make regression/smoke`
    
    Removes all of the unused and extraneous targets, macros
    and variables from the Makefile. These can be reintroduced
    later if needed
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- regression/daily/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/hyperledger/fabric-test/regression/dailytest
-
-go 1.13
-
-require (
-	github.com/hyperledger/fabric-test v1.1.1-0.20191126160459-3b39434023ee
-	github.com/onsi/ginkgo v1.10.3
-	github.com/onsi/gomega v1.7.1
-)
--- scripts/pre_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-set -o pipefail
-
-########################################################
-# Updating go modules for all the fabric-test chaincodes
-########################################################
-cd $PWD/scripts
-./update_go_modules.sh
