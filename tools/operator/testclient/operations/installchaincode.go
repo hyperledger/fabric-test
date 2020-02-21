425e2b85981be3ca3cbd18421b84e284f14ba446
Surya Lanka <suryalnvs@gmail.com>
2020-02-21 16:58:00 -0500

    [FAB-17503] Add install and commit using new lifecycle in operator
    Adding packaging and installing chaincode functions to
    installchaincode.go, approveformyorg and committing to
    instantiatechaincode.go which will be called when `sdk: cli`
    is called in install and instantiate in test input file
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>

--- tools/operator/testclient/operations/installchaincode.go
+++ tools/operator/testclient/operations/installchaincode.go
@@ -3,6 +3,7 @@ package operations
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -14,6 +15,7 @@ import (
 
 //InstallCCUIObject --
 type InstallCCUIObject struct {
+	SDK             string             `json:"sdk,omitempty"`
 	TransType       string             `json:"transType,omitempty"`
 	TLS             string             `json:"TLS,omitempty"`
 	ChainCodeID     string             `json:"chaincodeID,omitempty"`
@@ -51,7 +53,7 @@ func (i InstallCCUIObject) createInstallCCObjects(ccObject inputStructs.InstallC
 	var installCCObjects []InstallCCUIObject
 	var channelOpt ChannelOptions
 	var deployOpt InstallCCDeployOpt
-	i = InstallCCUIObject{TransType: "install", TLS: tls, ChainCodeVer: ccObject.ChainCodeVersion, ChainCodeID: ccObject.ChainCodeName}
+	i = InstallCCUIObject{SDK: ccObject.SDK, TransType: "install", TLS: tls, ChainCodeVer: ccObject.ChainCodeVersion, ChainCodeID: ccObject.ChainCodeName}
 	orgNames := strings.Split(ccObject.Organizations, ",")
 	channelOpt = ChannelOptions{OrgName: orgNames}
 	deployOpt = InstallCCDeployOpt{ChainCodePath: ccObject.ChainCodePath, Language: ccObject.Language}
@@ -63,6 +65,87 @@ func (i InstallCCUIObject) createInstallCCObjects(ccObject inputStructs.InstallC
 	return installCCObjects
 }
 
+//SetEnvForCLI -- sets environment variables for running peer cli commands
+func SetEnvForCLI(orgName, connProfilePath, tls, currentDir string) error {
+
+	var tlsNetwork string
+	mspID, err := GetMSPIDForOrg(connProfilePath, orgName)
+	if err != nil {
+		return err
+	}
+
+	if tls == "clientauth" {
+		tlsNetwork = "true"
+	} else {
+		tlsNetwork = "false"
+	}
+
+	os.Setenv("CORE_PEER_LOCALMSPID", mspID)
+	os.Setenv("CORE_PEER_TLS_ENABLED", tlsNetwork)
+	os.Setenv("CORE_PEER_MSPCONFIGPATH", fmt.Sprintf("%s/crypto-config/peerOrganizations/%s/users/Admin@%s/msp", currentDir, orgName, orgName))
+	os.Setenv("CORE_PEER_TLS_ROOTCERT_FILE", fmt.Sprintf("%s/crypto-config/peerOrganizations/%s/peers/peer0-%s.%s/tls/ca.crt", currentDir, orgName, orgName, orgName))
+	return nil
+}
+
+//packageCC -- package cc using cli
+func (i InstallCCUIObject) packageCC(installObject InstallCCUIObject) error {
+
+	currentDir, err := paths.GetCurrentDir()
+	if err != nil {
+		return err
+	}
+	err = SetEnvForCLI(installObject.ChannelOpt.OrgName[0], installObject.ConnProfilePath, installObject.TLS, currentDir)
+	if err != nil {
+		return err
+	}
+
+	args := []string{"lifecycle",
+		"chaincode",
+		"package",
+		"cc.tgz",
+		"--path", installObject.DeployOpt.ChainCodePath,
+		"--label", fmt.Sprintf("%s_%s", installObject.ChainCodeID, installObject.ChainCodeVer)}
+
+	_, err = networkclient.ExecuteCommand("peer", args, true)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+//installCCusingCLI -- installing cc using cli
+func (i InstallCCUIObject) installCCusingCLI(installObject InstallCCUIObject) error {
+
+	orgName := installObject.ChannelOpt.OrgName[0]
+	currentDir, err := paths.GetCurrentDir()
+	if err != nil {
+		return err
+	}
+	err = SetEnvForCLI(orgName, installObject.ConnProfilePath, installObject.TLS, currentDir)
+	if err != nil {
+		return err
+	}
+
+	args := []string{"lifecycle",
+		"chaincode",
+		"install",
+		"cc.tgz",
+		"--peerAddresses", "127.0.0.1:31000",
+		"--tlsRootCertFiles", fmt.Sprintf("%s/crypto-config/peerOrganizations/%s/peers/peer0-%s.%s/tls/ca.crt", currentDir, orgName, orgName, orgName),
+		// "--connectionProfile",
+		// installObject.ConnProfilePath,
+	}
+
+	if installObject.TLS == "clientauth" {
+		args = append(args, "--tls")
+	}
+	_, err = networkclient.ExecuteCommand("peer", args, true)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
 //installCC -- To install chaincode
 func (i InstallCCUIObject) installCC(installCCObjects []InstallCCUIObject) error {
 
@@ -70,15 +153,26 @@ func (i InstallCCUIObject) installCC(installCCObjects []InstallCCUIObject) error
 	var jsonObject []byte
 	pteMainPath := paths.PTEPath()
 	for j := 0; j < len(installCCObjects); j++ {
-		jsonObject, err = json.Marshal(installCCObjects[j])
-		if err != nil {
-			return err
-		}
-		startTime := fmt.Sprintf("%s", time.Now())
-		args := []string{pteMainPath, strconv.Itoa(j), string(jsonObject), startTime}
-		_, err = networkclient.ExecuteCommand("node", args, true)
-		if err != nil {
-			return err
+		if installCCObjects[j].SDK == "cli" {
+			err = i.packageCC(installCCObjects[j])
+			if err != nil {
+				return err
+			}
+			err = i.installCCusingCLI(installCCObjects[j])
+			if err != nil {
+				return err
+			}
+		} else {
+			jsonObject, err = json.Marshal(installCCObjects[j])
+			if err != nil {
+				return err
+			}
+			startTime := fmt.Sprintf("%s", time.Now())
+			args := []string{pteMainPath, strconv.Itoa(j), string(jsonObject), startTime}
+			_, err = networkclient.ExecuteCommand("node", args, true)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return err
