e7ad5e1b305260ba08cd68265f213615ed16fa1c
Brett Logan <brett.t.logan@ibm.com>
2020-04-09 10:32:26 -0400

    Update 2.1 Branch Version
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/scripts/build/fabric-chaincode-java.sh
+++ ci/scripts/build/fabric-chaincode-java.sh
@@ -7,6 +7,3 @@ set -euo pipefail
 git clone -b ${BRANCH} https://github.com/hyperledger/fabric-chaincode-java "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-java"
 cd "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-java"
 ./gradlew buildImage -x test
-
-# This is temporary until chaincode-java starts adding 2 digit tags
-docker tag hyperledger/fabric-javaenv hyperledger/fabric-javaenv:2.1
