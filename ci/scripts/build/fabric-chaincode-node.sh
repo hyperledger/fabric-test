4f52653db01d91c589970596fb93cd9edf502bfd
Brett Logan <brett.t.logan@ibm.com>
2020-04-08 23:20:15 -0400

    Remove 2.0 publishing
    Chaincode Node and Java no longer have release branches,
    changes are now put into the next minor release
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/scripts/build/fabric-chaincode-node.sh
+++ ci/scripts/build/fabric-chaincode-node.sh
@@ -4,10 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
 
-git clone -b ${BRANCH} https://github.com/hyperledger/fabric-chaincode-node "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-node"
-cd "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-node"
-node common/scripts/install-run-rush.js install
-node common/scripts/install-run-rush.js rebuild
-
-# This is temporary until chaincode-node starts adding 2 digit tags
-docker tag hyperledger/fabric-nodeenv hyperledger/fabric-nodeenv:2.1
+docker pull hyperledger/fabric-nodeenv:2.0
+docker tag hyperledger/fabric-nodeenv:2.0 hyperledger/fabric-nodeenv:2.1
