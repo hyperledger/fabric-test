9da04c784c183ed0b8342bd761e82504c7cc4824
Brett Logan <brett.t.logan@ibm.com>
2020-11-29 12:38:52 -0500

    Remove Long Running Tests
    Both the production and chaos suites need to be completely
    refactored. In particular, the production suite needs to be
    migrated to chaincodes that excersize more realistic production
    functionality of fabric such as complex queries and private data.
    
    The chaos suite needs to be reimplemented to do actual process
    cancellation, i.e., `kubectl exec -it <pod name> -- kill -9 0`
    this will do a forceful cancellation rather than allowing for
    a graceful shutdown as even a forceful `kubectl delete -f` only
    removes the pod from the API server registration and does not
    kill the pod.
    
    We will revisit the implemention in a later Jira.
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- scripts/pullBinaries.sh
+++ scripts/pullBinaries.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 # Set the working directory
-WD=$(cd $(dirname $0)/.. && pwd)
-cd ${WD}
+WD=$(cd "$(dirname "$0")/.." && pwd)
+cd "${WD}"
 
 RELEASE_VERSION=$1
 shift
@@ -19,20 +19,20 @@ else
     exit 1
 fi
 
-printf "\nDownloading fabric-${ARCH} binaries from Artifactory\n"
+printf "\nDownloading fabric-%s binaries from Artifactory\n" "${ARCH}"
 
 ##########################################################
 # Pull the binaries from Artifactory
 ##########################################################
 
-REPOS=$@
-mkdir -p ${WD}/bin
-for repo in "${REPOS}"; do
+REPOS=("$@")
+mkdir -p "${WD}/bin"
+for repo in "${REPOS[@]}"; do
 	ARTIFACTORY_URL=https://hyperledger.jfrog.io/hyperledger/fabric-binaries/hyperledger-${repo}-${ARCH}-${RELEASE_VERSION}.tar.gz
 	curl -sS "${ARTIFACTORY_URL}" -o binaries.tgz
 	tar -xf binaries.tgz
 	rm -rf binaries.tgz
-	printf "\nFinished downloading $repo binaries\n\n"
+	printf "\nFinished downloading %s binaries\n\n" "${repo}"
 done
 
-printf "The following binaries have been installed at ${WD}/bin:\n\n%s\n\n" "$(ls -l ${WD}/bin)"
+printf "The following binaries have been installed at %s/bin:\n\n%s\n\n" "${WD}" "$(ls -l "${WD}"/bin)"
