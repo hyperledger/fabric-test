1276f86d30ea6a994ce6ce5187f181c83487c340
Brett Logan <brett.t.logan@ibm.com>
2021-01-22 09:19:11 -0500

    Update branch version
    The chaincodes and CA do not have release branches
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/scripts/interop/build/fabric-chaincode-java.sh
+++ ci/scripts/interop/build/fabric-chaincode-java.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
 
-git clone -b ${BRANCH} https://github.com/hyperledger/fabric-chaincode-java --single-branch "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-java"
+git clone -b release-2.2 https://github.com/hyperledger/fabric-chaincode-java --single-branch "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-java"
 cd "${GOPATH}/src/github.com/hyperledger/fabric-chaincode-java"
 ./gradlew buildImage -x javadoc -x test -x checkstyleMain -x checkstyleTest -x dependencyCheckAnalyze
 
