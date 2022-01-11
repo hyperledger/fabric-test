0de2d466ef9ed5ff0e0ce47c7682695234186a17
D <d_kelsey@uk.ibm.com>
2022-01-11 10:32:33 +0000

    Chaincode used for chaos testing
    Signed-off-by: D <d_kelsey@uk.ibm.com>

---
+++ chaincodes/chaos/node/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ChaosAssetContract } from './chaos-asset-contract';
+export { ChaosAssetContract } from './chaos-asset-contract';
+
+export const contracts: any[] = [ ChaosAssetContract ];
