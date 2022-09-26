56d8c1f528275fba7139a09bb5b896b295b94b59
David Enyeart <enyeart@us.ibm.com>
2022-09-25 23:23:19 -0400

    Make fabric-nodeenv version match fabric version
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- ci/scripts/interop/driver/fabric-chaincode-node.sh
+++ ci/scripts/interop/driver/fabric-chaincode-node.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 cd "${ARTIFACT_DIRECTORY}/nodeenv-source"
 tar -xf nodeenv-source.tgz
 sed -i 's/.\/tools\/getEdgeDocker.sh/docker images/g' common/config/rush/command-line.json
+docker tag hyperledger/fabric-nodeenv:latest hyperledger/fabric-nodeenv:3.0
 node common/scripts/install-run-rush.js install
 node common/scripts/install-run-rush.js rebuild
 node common/scripts/install-run-rush.js update
