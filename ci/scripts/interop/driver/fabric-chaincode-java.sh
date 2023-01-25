e0ab1746747b42a683b50fdcb1b1439107ff600c
Matthew B White <whitemat@uk.ibm.com>
2023-01-25 15:21:01 +0000

    Copy the intergation tests github action from chaincodejava
    If (when) fabric-test is in github actions it could pull the integration pipeline directly
    
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- ci/scripts/interop/driver/fabric-chaincode-java.sh
+++ ci/scripts/interop/driver/fabric-chaincode-java.sh
@@ -7,4 +7,18 @@ set -euo pipefail
 cd "${ARTIFACT_DIRECTORY}/javaenv-source"
 tar -xf javaenv-source.tgz
 
-./gradlew build -x getLatestDockerImages -x buildImage -x checkstyleMain -x checkstyleTest -x dependencyCheckAnalyze -x javadoc
+export GITHUB_WORKSPACE=$(pwd)
+# 
+# copy of the integrationtest github action
+./gradlew -I $GITHUB_WORKSPACE/fabric-chaincode-integration-test/chaincodebootstrap.gradle -PchaincodeRepoDir=$GITHUB_WORKSPACE/fabric-chaincode-integration-test/src/contracts/fabric-shim-api/repository publishShimPublicationToFabricRepository
+./gradlew -I $GITHUB_WORKSPACE/fabric-chaincode-integration-test/chaincodebootstrap.gradle -PchaincodeRepoDir=$GITHUB_WORKSPACE/fabric-chaincode-integration-test/src/contracts/fabric-ledger-api/repository publishShimPublicationToFabricRepository
+./gradlew -I $GITHUB_WORKSPACE/fabric-chaincode-integration-test/chaincodebootstrap.gradle -PchaincodeRepoDir=$GITHUB_WORKSPACE/fabric-chaincode-integration-test/src/contracts/bare-gradle/repository publishShimPublicationToFabricRepository
+./gradlew -I $GITHUB_WORKSPACE/fabric-chaincode-integration-test/chaincodebootstrap.gradle -PchaincodeRepoDir=$GITHUB_WORKSPACE/fabric-chaincode-integration-test/src/contracts/bare-maven/repository publishShimPublicationToFabricRepository
+./gradlew -I $GITHUB_WORKSPACE/fabric-chaincode-integration-test/chaincodebootstrap.gradle -PchaincodeRepoDir=$GITHUB_WORKSPACE/fabric-chaincode-integration-test/src/contracts/wrapper-maven/repository publishShimPublicationToFabricRepository
+
+
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh | bash -s -- binary
+npm install -g @hyperledger-labs/weft  
+
+
+./gradlew :fabric-chaincode-integration-test:build -xdependencyCheckAnalyze 
